15a776d1fe60c134efcc941a29208797853d7303
dh4rio <marzchelo1@gmail.com>
2026-08-05 16:01:14 -0500

    corrección refresh

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -18,7 +18,7 @@ export const authService = {
   },
 
   refreshToken: async (refresh: string) => {
-    const { data } = await axios.post(`${BASE_URL}/token/refresh/`, {
+    const { data } = await axios.post(`${BASE_URL}token/refresh/`, {
       refresh,
     });
 
